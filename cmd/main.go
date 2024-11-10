b437ee64f821eff9b8de765feae8c6e30d2512fa
Xaness <nasar165@hotmail.com>
2024-11-11 00:03:27 +0700

    add copyright

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,30 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/mcs-unity/ocpp-simulator/internal/charger"
 	"github.com/mcs-unity/ocpp-simulator/internal/exception"
 )
 
+func printCopyRight(path string) {
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return
+	}
+	fmt.Println(string(b))
+	fmt.Printf("© %d MCS Unity Co.,LTD - copyright all rights reserved\n", time.Now().Year())
+}
+
 func main() {
 	defer exception.Exception()
 	args := os.Args
+
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	printCopyRight(fmt.Sprintf("%s/resources/logo.txt", dir))
 
 	if len(args) < 3 {
 		panic("input arguments invalid please use command <websocket> <connectors>")
